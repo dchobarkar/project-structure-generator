5fe6ed191149c989a85dc7bd776a21866d19cb7a
Darshan Chobarkar <darshan@trubotai.com>
2026-02-01 19:58:46 +0530

    feat: add templates

---
+++ templates/nextFeatureTemplate.ts
@@ -0,0 +1,11 @@
+import type { FolderTree } from "@/types/generator";
+
+export const nextFeatureTemplate: FolderTree = {
+  app: {},
+  components: {},
+  modules: {},
+  lib: {},
+  hooks: {},
+  services: {},
+  types: {},
+};
